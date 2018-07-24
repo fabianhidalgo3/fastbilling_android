130764d412127fb189afad6f2fd09d0712645e3c
Fabian Hidalgo <fabianhidalgo3@gmail.com>
2018-07-24 14:50:41 -0400

    se agregan nuevos campos al modelo observacion, metodos get set y se incorporan al contructor

--- app/src/main/java/modelos/Observacion.java
+++ app/src/main/java/modelos/Observacion.java
@@ -6,6 +6,7 @@ import static android.provider.BaseColumns._ID;
 
 /**
  * Created by brayan on 22-06-17.
+ * Modifiqued by Fabián Hidalgo
  */
 
 public class Observacion implements Serializable
@@ -13,43 +14,55 @@ public class Observacion implements Serializable
     private int id;
     private String descripcion;
     private int claveLecturaId;
+    private int num_fotografias;
+    private boolean requerido;
+    private boolean efectivo;
+    private boolean factura;
+    private boolean folio;
 
     public Observacion(){}
 
-    public Observacion(int id, String descripcion, int clave_lectura_id)
-    {
+
+    public Observacion(int id, String descripcion, int claveLecturaId, int num_fotografias, boolean requerido, boolean efectivo, boolean factura, boolean folio) {
         this.id = id;
         this.descripcion = descripcion;
-        this.claveLecturaId = clave_lectura_id;
+        this.claveLecturaId = claveLecturaId;
+        this.num_fotografias = num_fotografias;
+        this.requerido = requerido;
+        this.efectivo = efectivo;
+        this.factura = factura;
+        this.folio = folio;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
 
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
-    public String getDescripcion()
-    {
+    public String getDescripcion() {
         return descripcion;
     }
 
-    public void setDescripcion(String descripcion)
-    {
-        this.descripcion = descripcion;
-    }
-
-    public int getClaveLecturaId()
-    {
+    public int getClaveLecturaId() {
         return claveLecturaId;
     }
 
-    public void setClaveLecturaId(int claveLecturaId)
-    {
-        this.claveLecturaId = claveLecturaId;
+    public int getNum_fotografias() {
+        return num_fotografias;
+    }
+
+    public boolean isRequerido() {
+        return requerido;
+    }
+
+    public boolean isEfectivo() {
+        return efectivo;
+    }
+
+    public boolean isFactura() {
+        return factura;
+    }
+
+    public boolean isFolio() {
+        return folio;
     }
 }
